30113a50b20f7da10d74d0706f157a0942a89be8
Anant <anant.ahirrao4@gmail.com>
2022-04-12 13:04:01 +0530

    Passing data from child to parent

--- learn/src/app/app.component.ts
+++ learn/src/app/app.component.ts
@@ -12,4 +12,7 @@ export class AppComponent {
     alert('Heyy');
   }
 
+  setName(data){
+    console.warn(data);
+  }
 }
--- learn/src/app/first-comp/first-comp.component.ts
+++ learn/src/app/first-comp/first-comp.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-first-comp',
@@ -7,9 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FirstCompComponent implements OnInit {
 
+  @Output() setname:EventEmitter<any>=new EventEmitter()
   constructor() { }
 
   ngOnInit(): void {
+    this.setname.emit('from child')
   }
 
 }
