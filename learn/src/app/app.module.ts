6bea6756d96a453d3e15ff2cb75500f331927564
Anant <anant.ahirrao4@gmail.com>
2022-04-14 17:42:41 +0530

    Lazy loding is done on admin module

--- learn/src/app/app.module.ts
+++ learn/src/app/app.module.ts
@@ -3,7 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AdminRoutingModule } from './admin/admin-routing.module';
 import { UserRoutingModule } from './user/user-routing.module'
 
 @NgModule({
@@ -13,7 +12,6 @@ import { UserRoutingModule } from './user/user-routing.module'
   imports: [
     BrowserModule,
     AppRoutingModule,
-    AdminRoutingModule,
     UserRoutingModule
   ],
   providers: [],
