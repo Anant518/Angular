e6485bda5b40e989373ac4ee19419429f81f1b9c
Anant <anant.ahirrao4@gmail.com>
2022-04-18 14:09:00 +0530

    Added reactive form

--- learn/src/app/app.component.ts
+++ learn/src/app/app.component.ts
@@ -7,22 +7,7 @@ import { FormGroup, FormControl, FormControlName } from '@angular/forms'
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Welcome to angular';
-  name = 'My Name is Anant';
-
-  getName() {
-    return this.name;
-  }
-
-  obj = {
-    name: 'Passing Array name',
-    roll: 234
-  }
-
-  arr = ['anx', 'xyz', 'me']
-
-  siteUrl = window.location.href
-  onsubmit(data) {
+   onsubmit(data) {
     console.warn(data)
   }
 
